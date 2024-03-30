a4b4e1194734c92fc020d1692f68cabf7bb23ddb
fabián Reyes <fareyesinostroza@gmail.com>
2024-03-29 21:40:24 -0300

    first commit

---
+++ src/components/Alert.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BootstrapAlert from "react-bootstrap/Alert";
+
+function CustomAlert({ message, variant }) {
+  return <BootstrapAlert variant={variant}>{message}</BootstrapAlert>;
+}
+
+export default CustomAlert;
